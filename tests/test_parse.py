38df0d083109356eef1f110fadfbab6cb9ec00d1
Mark <mark.hall@work.room3b.eu>
2020-04-18 21:40:07 +0100

    Brought tests in line

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -2,20 +2,20 @@ from pwh_permissions import tokenise, parse
 
 
 def test_basic_parse():
-    instructions = parse(tokenise('$obj allow $user "edit"'))
+    instructions = parse(tokenise('obj allow user edit'))
     assert len(instructions) == 1
-    assert instructions == [('$obj', 'allow', '$user', '"edit"')]
+    assert instructions == [('obj', 'allow', 'user', 'edit')]
 
 
 def test_combined_parse():
-    instructions = parse(tokenise('$obj allow $user "edit" or $obj has_permission "admin"'))
+    instructions = parse(tokenise('obj allow user edit or user has_role admin'))
     assert len(instructions) == 3
-    assert instructions == [('$obj', 'allow', '$user', '"edit"'), ('$obj', 'has_permission', '"admin"'), 'or']
+    assert instructions == [('obj', 'allow', 'user', 'edit'), ('user', 'has_role', 'admin'), 'or']
 
 
 def test_bracket_parse():
-    instructions = parse(tokenise('$obj allow $user "edit" and ($obj has_permission "admin" or $obj has_permission ' +
-                                  '"superuser")'))
+    instructions = parse(tokenise('obj allow user edit and (user has_role admin or user has_role ' +
+                                  'superuser)'))
     assert len(instructions) == 5
-    assert instructions == [('$obj', 'allow', '$user', '"edit"'), ('$obj', 'has_permission', '"admin"'),
-                            ('$obj', 'has_permission', '"superuser"'), 'or', 'and']
+    assert instructions == [('obj', 'allow', 'user', 'edit'), ('user', 'has_role', 'admin'),
+                            ('user', 'has_role', 'superuser'), 'or', 'and']
--- tests/test_tokenise.py
+++ tests/test_tokenise.py
@@ -2,19 +2,19 @@ from pwh_permissions import tokenise
 
 
 def test_basic_tokenise():
-    tokens = tokenise('$obj allow $user "edit"')
+    tokens = tokenise('obj allow user edit')
     assert len(tokens) == 4
-    assert tokens == ['$obj', 'allow', '$user', '"edit"']
+    assert tokens == ['obj', 'allow', 'user', 'edit']
 
 
 def test_combined_tokenise():
-    tokens = tokenise('$obj allow $user "edit" or $obj has_permission "admin"')
+    tokens = tokenise('obj allow user edit or user has_role admin')
     assert len(tokens) == 8
-    assert tokens == ['$obj', 'allow', '$user', '"edit"', 'or', '$obj', 'has_permission', '"admin"']
+    assert tokens == ['obj', 'allow', 'user', 'edit', 'or', 'user', 'has_role', 'admin']
 
 
 def test_bracket_tokenise():
-    tokens = tokenise('$obj allow $user "edit" and ($obj has_permission "admin" or $obj has_permission "superuser")')
+    tokens = tokenise('obj allow user edit and (user has_role admin or user has_role superuser)')
     assert len(tokens) == 14
-    assert tokens == ['$obj', 'allow', '$user', '"edit"', 'and', '(', '$obj', 'has_permission', '"admin"', 'or', '$obj',
-                      'has_permission', '"superuser"', ')']
+    assert tokens == ['obj', 'allow', 'user', 'edit', 'and', '(', 'user', 'has_role', 'admin', 'or', 'user',
+                      'has_role', 'superuser', ')']
